8d53e27ceda6dc219c0e3d87c32c97f4e54f8fd8
GlennD12 <glenndalemagbanua@gmail.com>
2025-03-31 16:58:03 +0800

    msw fix

--- src/__tests__/MockAPICalls.spec.tsx
+++ src/__tests__/MockAPICalls.spec.tsx
@@ -5,9 +5,6 @@ import { render, screen, waitFor } from '@testing-library/react'
 import { FakeProducts } from '../components/FakeProducts/FakeProducts'
 
 const server = setupServer(
-  // Describe network behavior with request handlers.
-  // Tip: move the handlers into their own module and
-  // import it across your browser and Node.js setups!
   rest.get('https://fakestoreapi.com/products', (req , res, ctx) => {
     console.log(req);
     return res(
@@ -19,45 +16,24 @@ const server = setupServer(
             description: "test",
             category: "test",
             image: "test"
-            },
-        {
-            id: 1,
-            title: "tes1",
-            price: 0.11,
-            description: "test1",
-            category: "test1",
-            image: "test1"
         },
       ]),
     )
   }),
 )
 
-// Enable request interception.
-beforeAll(() => server.listen())
 
-// Reset handlers so that each test could alter them
-// without affecting other, unrelated tests.
-afterEach(() => server.resetHandlers())
-
-// Don't forget to clean up afterwards.
-afterAll(() => server.close())
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 it('displays the list of recent posts', async () => {
   render(<FakeProducts />)
 
-//   🕗 Wait for the posts request to be finished.
   await waitFor(() => {
     expect(
       screen.getByText("Posts:")).toBeInTheDocument();
   })
 
-  // ✅ Assert that the correct posts have loaded.
   expect(screen.getByTestId("test")).toBeInTheDocument()
-//   const textBoxElement = screen.getAllByRole("textbox");
-//   expect(textBoxElement).toBeInTheDocument()
-
-//   expect(
-//     screen.getByTestId("test1"),
-//   ).toBeInTheDocument()
 })
